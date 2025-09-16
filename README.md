8f3b77158614bfe055dea6570f59e38db7608475
LamarAlSubhi <lamarmaalsubhi@gmail.com>
2025-09-16 19:07:13 +0300

    implemented doMap (seems to be working but unsure)

--- README.md
+++ README.md
@@ -3,7 +3,65 @@
 ## as1: Sequential MapReduce
 notes for myself:
 
-steps for map:
-1) it reads one of the input files (inFile)
-2) calls the user-defined map function (mapF) for that file's contents
-3) partitions the output into nReduce intermediate files.
+- Partition rule: r = ihash(key) % nReduce so all identical keys go to the same reducer
+- Intermediate files (per map task i): produce exactly nReduce files: f{i}-0, f{i}-1, …, f{i}-{nReduce-1}.
+
+- intermediate files = JSON KV records (map → reduce)
+- reduce outputs = plain text “key value” lines (reduce → final merge)
+
+- All KVs with r = 0 go into the *-0 file for each map task
+- Later, reduce task 0 will read all *-0 files across map tasks
+- That way, reducer 0 sees every KV that hashed to bucket 0
+
+
+
+
+## Map flow:
+
+- STEP 1: read the whole input file for this task
+- STEP 2: call the mapF on the content
+- STEP 3: split the pairs produced by STEP 2 using their reduce index r = hash(key) % nReduce. 
+- STEP 4: for each single reduce index r, I add the pairs that belong to r into the intermediate file for r. 
+
+#### example:
+
+## inputs:
+- files: 
+    -  input1.txt: "foo bar foo" 
+    -  input2.txt: "bar baz"
+- mapF (wc): returns (word, "1") for each word
+- nReduce = 2 (so keys are split between r=0 and r=1)
+
+## DoMap on input1.txt
+- calls mapF: 
+    - mapF("input1.txt", "foo bar foo") → [("foo","1"), ("bar","1"), ("foo","1")]
+
+- partitions (using hash key):
+    - "foo" → reducer 0
+    - "bar" → reducer 1
+
+- writes n intermediate files:
+    - mr-0 (for reducer 0): {"Key":"foo","Value":"1"}{"Key":"foo","Value":"1"}
+    - mr-1 (for reducer 1): {"Key":"bar","Value":"1"}
+
+## DoMap on input2.txt
+- calls mapF: 
+    - mapF("input2.txt", "bar baz") → [("bar","1"), ("baz","1")]
+
+- partitions (using hash key):
+    - "baz" → reducer 0
+    - "bar" → reducer 1
+
+- writes n intermediate files (JSON objects, one per line):
+    - mr-2-0 (for reducer 0): {"Key":"bar","Value":"1"}
+    - mr-2-1 (for reducer 1): {"Key":"baz","Value":"1"}
+
+
+
+## REDUCE LATER
+
+
+
+
+
+
--- src/mapreduce/common_map.go
+++ src/mapreduce/common_map.go
@@ -1,7 +1,10 @@
 package mapreduce
 
 import (
+	"encoding/json" // for the json objects
+	"fmt"           // for printing
 	"hash/fnv"
+	"os" // so i can read and make files and stuff
 )
 
 // doMap does the job of a map worker: it reads one of the input files
@@ -14,32 +17,64 @@ func doMap(
 	nReduce int, // the number of reduce task that will be run ("R" in the paper)
 	mapF func(file string, contents string) []KeyValue,
 ) {
-	// TODO:
-	// You will need to write this function.
-	// You can find the filename for this map task's input to reduce task number
-	// r using reduceName(jobName, mapTaskNumber, r). The ihash function (given
-	// below doMap) should be used to decide which file a given key belongs into.
-	//
-	// The intermediate output of a map task is stored in the file
-	// system as multiple files whose name indicates which map task produced
-	// them, as well as which reduce task they are for. Coming up with a
-	// scheme for how to store the key/value pairs on disk can be tricky,
-	// especially when taking into account that both keys and values could
-	// contain newlines, quotes, and any other character you can think of.
-	//
-	// One format often used for serializing data to a byte stream that the
-	// other end can correctly reconstruct is JSON. You are not required to
-	// use JSON, but as the output of the reduce tasks *must* be JSON,
-	// familiarizing yourself with it here may prove useful. You can write
-	// out a data structure as a JSON string to a file using the commented
-	// code below. The corresponding decoding functions can be found in
-	// common_reduce.go.
-	//
-	//   enc := json.NewEncoder(file)
-	//   for _, kv := ... {
-	//     err := enc.Encode(&kv)
-	//
-	// Remember to close the file after you have written all the values!
+
+	// STEP 1: read the whole input file for this task
+
+	debug("[DOMAP #%d] starting", mapTaskNumber)
+
+	bytes, err := os.ReadFile(inFile) // returns byte []
+
+	if err != nil {
+		fmt.Printf("[DOMAP]error reading file [%s]: %v\n", inFile, err)
+		// im thorough, arent i ;)
+	}
+	content := string(bytes) // need as string for mapF
+
+	debug("[DOMAP] read %d bytes from %s\n", len(content), inFile)
+
+	// STEP 2: call the mapF on the content
+	kvs := mapF(inFile, content)
+	debug("[DOMAP] mapF produced %d kvs for %s\n", len(kvs), inFile)
+	if len(kvs) > 0 {
+		debug("[DOMAP] sample kv: %q -> %q\n", kvs[0].Key, kvs[0].Value)
+	}
+
+	// STEP 3: split the pairs produced by STEP 2 using their reduce index r = hash(key) % nReduce.
+
+	// loop prep, create files
+	files := make([]*os.File, nReduce) // one intermediate file per reducer index r for each map task
+	encs := make([]*json.Encoder, nReduce)
+
+	// lamda func to close the files at the end
+	defer func() {
+		for _, f := range files {
+			if f != nil {
+				_ = f.Close()
+			}
+		}
+	}()
+
+	for _, kv := range kvs {
+		r := int(ihash(kv.Key)) % nReduce
+		if encs[r] == nil {
+			rname := reduceName(jobName, mapTaskNumber, r) // get file name
+			f, err := os.Create(rname)                     // creating the actual file
+
+			if err != nil {
+				fmt.Printf("[DOMAP] error creating file [%s]: %v\n", rname, err)
+			}
+
+			files[r] = f
+			encs[r] = json.NewEncoder(f)
+		}
+		// STEP 4: for each single reduce index r, I add the pairs that belong to r into the intermediate file for r.
+		err := encs[r].Encode(&kv)
+
+		if err != nil {
+			fmt.Printf("[DOMAP] error encoding file %v\n", err)
+		}
+	}
+	debug("[DOMAP #%d] successfully completed", mapTaskNumber)
 }
 
 func ihash(s string) uint32 {
