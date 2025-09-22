09424393090b67eac031ad6836abffe56dd99332
LamarAlSubhi <lamarmaalsubhi@gmail.com>
2025-09-22 17:55:56 +0300

    as2: finished part 1 and 2 yay

--- as2.md
+++ as2.md
@@ -1,1 +1,20 @@
 ## as2: Distributed MapReduce
+
+- The master tells the worker about a new task by sending it the RPC call Worker.DoTask, giving a DoTaskArgs object as the RPC argument.
+
+- The master may have to wait for a worker to finish before it can hand out more tasks. You may find channels useful to synchronize threads that are waiting for reply with the master once the reply arrives. Channels are explained in the document on Concurrency in Go.
+
+
+- mr.registerChannel: new worker args
+- mr.workers: string array of workers
+
+
+- ok := call(mr.address, "Master.{FUNCNAME}", &args, &reply)
+
+- STEP1: grab woker from channel
+- STEP2: send worker an RPC to DoTask
+- STEP3: only after success, return worker to channel, otherwise, retry by looping again
+- STEP4: after all tasks in phase are done, return
+
+
+
--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -13,14 +13,51 @@ func (mr *Master) schedule(phase jobPhase) {
 		nios = len(mr.files)
 	}
 
+	// im making a channel that will be populated with true for every done task
+	doneChan := make(chan bool, ntasks)
+
+	file := "" // initializing file arg for DoTaskArgs struct
+
 	debug("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, nios)
 
-	// All ntasks tasks have to be scheduled on workers, and only once all of
-	// them have been completed successfully should the function return.
-	// Remember that workers may fail, and that any given worker may finish
-	// multiple tasks.
-	//
-	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-	//
+	for task := 0; task < ntasks; task++ {
+		task := task // rebind loop var so that the go routines get correct task
+
+		go func() {
+
+			// setting up DoTaskArgs
+			if phase == mapPhase {
+				// use the task number as index for files
+				file = mr.files[task]
+			}
+			args := DoTaskArgs{JobName: mr.jobName, File: file, Phase: phase, TaskNumber: task, NumOtherPhase: nios}
+			reply := new(struct{}) // just emptyx
+
+			for {
+				// 	STEP1: grab woker from channel
+				worker := <-mr.registerChannel
+
+				// STEP2: send worker an RPC to DoTask
+				debug("calling worker %v\n", worker)
+				ok := call(worker, "Worker.DoTask", &args, reply)
+
+				if ok {
+					// STEP3: only after success, return worker to channel
+					go func() { mr.registerChannel <- worker }()
+					doneChan <- true
+					return
+				}
+				// otherwise, retry by looping again
+				debug("worker %v failed task %v, retrying...\n", worker, args.TaskNumber)
+			}
+		}()
+	}
+
+	// STEP4: after all tasks in phase are done, return
+	// outside for loop and go routines
+	for i := 0; i < ntasks; i++ {
+		<-doneChan
+	}
+
 	debug("Schedule: %v phase done\n", phase)
 }
