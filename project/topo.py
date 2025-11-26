133b8ae6480bcf828b98f57d10f61c88ddebf24d
LamarAlSubhi <lamarmaalsubhi@gmail.com>
2025-11-26 18:15:05 +0300

    added Linux gossipd binary in bin/linux/ for mininet environment, finally got everything configured correctly

--- project/topo.py
+++ project/topo.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 from mininet.topo import Topo
-from mininet.node import Controller
 from mininet.net import Mininet
+from mininet.node import OVSBridge
 from mininet.link import TCLink
 from mininet.cli import CLI
 
@@ -12,19 +12,24 @@ class GossipTopo(Topo):
         h2 = self.addHost('h2')
         h3 = self.addHost('h3')
 
-        s1 = self.addSwitch('s1')
+        # Force OVSBridge (learning switch, no OpenFlow)
+        s1 = self.addSwitch('s1', cls=OVSBridge)
 
-        self.addLink(h1, s1, cls=TCLink, delay='10ms')
-        self.addLink(h2, s1, cls=TCLink, delay='10ms')
-        self.addLink(h3, s1, cls=TCLink, delay='10ms')
+        self.addLink(h1, s1, delay='10ms')
+        self.addLink(h2, s1, delay='10ms')
+        self.addLink(h3, s1, delay='10ms')
 
 if __name__ == '__main__':
     topo = GossipTopo()
+
+    # Force switch type to OVSBridge and remove controller entirely
     net = Mininet(
         topo=topo,
-        controller=lambda name: Controller("c0"),
+        controller=None,
+        switch=OVSBridge,
         link=TCLink
     )
+
     net.start()
     CLI(net)
     net.stop()
