e00344fda742d5cf26de7e17c9b8418e69270e5b
LamarAlSubhi <lamarmaalsubhi@gmail.com>
2025-11-26 16:44:19 +0300

    trying to work on mininet

--- project/topo.py
+++ project/topo.py
@@ -1,27 +1,30 @@
+#!/usr/bin/env python3
+
 from mininet.topo import Topo
+from mininet.node import Controller
 from mininet.net import Mininet
-from mininet.node import OVSController
 from mininet.link import TCLink
 from mininet.cli import CLI
 
 class GossipTopo(Topo):
     def build(self):
-        # 3 hosts
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
         h3 = self.addHost('h3')
 
-        # Single switch
         s1 = self.addSwitch('s1')
 
-        # Links
         self.addLink(h1, s1, cls=TCLink, delay='10ms')
         self.addLink(h2, s1, cls=TCLink, delay='10ms')
         self.addLink(h3, s1, cls=TCLink, delay='10ms')
 
 if __name__ == '__main__':
     topo = GossipTopo()
-    net = Mininet(topo=topo, controller=OVSController, link=TCLink)
+    net = Mininet(
+        topo=topo,
+        controller=lambda name: Controller("c0"),
+        link=TCLink
+    )
     net.start()
     CLI(net)
     net.stop()
